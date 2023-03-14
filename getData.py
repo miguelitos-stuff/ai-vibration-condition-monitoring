5b5365e8b129dcd25029032dd1e6fe80f6986373
bartdewaart <bartdewaart@gmail.com>
2023-03-14 12:31:35 +0100

    Added one comment line

--- getData.py
+++ getData.py
@@ -4,3 +4,4 @@ import scipy.io
 
 #Use a for loop to make lists for every sensor on every gearbox called "(D/H)(1-10)AN(1-10)_list"
 #Look at week 1 AI course assignment for inspiration on how to create a for loop with several path names
+#listname = scipy.io.loadmat(pathname)
