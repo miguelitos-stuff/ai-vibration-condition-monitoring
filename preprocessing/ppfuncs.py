de5589f24d9661632e3af0a4255912cfb5e791db
flori <florian.mellab@gmail.com>
2023-03-16 15:54:41 +0100

    Added preprocessing file

--- preprocessing/ppfuncs.py
+++ preprocessing/ppfuncs.py
@@ -24,10 +24,7 @@ def generate_sample():
 
 
 #Making the matrices
-def visualize(matrix):
-    #Visualize the data matrix
-    test_matrix = np.random.randint(low=0, high=256, size=(224, 224))
-    visualize(test_matrix)
+
 
 
 
