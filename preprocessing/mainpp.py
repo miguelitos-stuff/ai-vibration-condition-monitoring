cc2785fb1537e2aad8b3f837944c1f32b0fb28a5
flori <florian.mellab@gmail.com>
2023-03-16 15:35:29 +0100

    Added preprocessing file

--- preprocessing/mainpp.py
+++ preprocessing/mainpp.py
@@ -1,1 +1,11 @@
 #Main file loop for preprocessin
+
+
+import matplotlib.pyplot as plt
+from ppfuncs import visualize
+import numpy as np
+
+
+#Visualize the data matrix
+test_matrix = np.random.randint(low=0, high=256, size=(224, 224))
+visualize(test_matrix)
---
+++ preprocessing/ppfuncs.py
@@ -0,0 +1,23 @@
+#Extracting the data
+
+
+
+#Making the matrices
+
+
+
+
+#Normalizing the data
+
+
+
+
+#Visualizing the normlaized data
+#Take the normalized data matrices (that have entries with values ranging from 0 to 255) and visualize these on a grey scale
+def visualize(data_matrix):
+    plt.imshow(data_matrix)
+    plt.show()
+
+    return
+
+
