c4fe4d0c5d8df28cf078f3daf223c68346399e2c
flori <florian.mellab@gmail.com>
2023-03-16 15:48:02 +0100

    Added preprocessing file

--- preprocessing/mainpp.py
+++ preprocessing/mainpp.py
@@ -12,7 +12,7 @@ def generate_sample():
 def normalize():
     pass
 
-def visualize(matrix):
-    #Visualize the data matrix
-    test_matrix = np.random.randint(low=0, high=256, size=(224, 224))
-    visualize(test_matrix)
+
+test_matrix = np.random.randint(low=0, high=256, size=(224, 224))
+visualize(test_matrix)
+print(test)
