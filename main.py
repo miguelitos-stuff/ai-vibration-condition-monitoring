6d0e20882e806dd3cea1052ec786ce523717c096
miguelitos-stuff <m.castrogracia@student.tudelft.nl>
2023-03-21 09:00:09 +0100

    Finished changes to mainpp

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import scipy.io
 import numpy as np
 
 # Make sure to put the file in the same directory as the pycharm project so the line below can operate smoothly.
-sensor_mat = scipy.io.loadmat('local_data/Damaged/D1.mat')
+sensor_mat = scipy.io.loadmat('data/Damaged/D1.mat')
 x = np.linspace(0, 60, 40000*60)
-
 # Choose the time interval you want to see and which sensor
 # time interval: 0 - 60 (seconds)
 # Sensors: AN3 to AN10
@@ -19,7 +18,8 @@ te = int(t_end * 40000) + 60025
 
 y = np.array(sensor_mat[sensor])
 y = y.flatten()
-
+print(y)
+print(sensor_mat)
 plt.figure()
 plt.plot(x[tb:te], y[tb:te])
 plt.show()
--- preprocessing/mainpp.py
+++ preprocessing/mainpp.py
@@ -4,11 +4,18 @@
 import matplotlib.pyplot as plt
 from ppfuncs import visualize
 import numpy as np
+from pathlib import Path
 
-
-<<<<<<< HEAD
-=======
 #Visualize the data matrix
-test_matrix = np.random.randint(low=0, high=256, size=(224, 224))
-visualize(test_matrix)
->>>>>>> e1831a46afb5e2077ba229e659e776abd9e9d068
+#test_matrix = np.random.randint(low=0, high=256, size=(224, 224))
+#visualize(test_matrix)
+
+#Make an array with all of the file numbers
+datapath = Path('../data/')
+filelist = []
+for i in datapath.iterdir():
+    for datafile in i.iterdir():
+        filelist.append(datafile)
+ordered_list = sorted(filelist, key = lambda x: x.stem[-6:-5])
+print(filelist)
+
