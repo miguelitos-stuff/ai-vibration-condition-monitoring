418e8ee6c21360a75a71356b85212a5cd23625f0
bartdewaart <bartdewaart@gmail.com>
2023-03-21 09:01:05 +0100

    New file

---
+++ cnn_architecture2.py
@@ -0,0 +1,52 @@
+# import the necessary packages
+from torch.nn import Module
+from torch.nn import Conv2d
+from torch.nn import Linear
+from torch.nn import MaxPool2d
+from torch.nn import ReLU
+from torch.nn import LogSoftmax
+from torch import flatten
+
+class LeNet(Module):
+	def __init__(self, numChannels, classes):
+		# call the parent constructor
+		super(LeNet, self).__init__()
+		# initialize first set of CONV => RELU => POOL layers
+		self.conv1 = Conv2d(in_channels=numChannels, out_channels=20,
+			kernel_size=(5, 5))
+		self.relu1 = ReLU()
+		self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+		# initialize second set of CONV => RELU => POOL layers
+		self.conv2 = Conv2d(in_channels=20, out_channels=50,
+			kernel_size=(5, 5))
+		self.relu2 = ReLU()
+		self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+		# initialize first (and only) set of FC => RELU layers
+		self.fc1 = Linear(in_features=800, out_features=500)
+		self.relu3 = ReLU()
+		# initialize our softmax classifier
+		self.fc2 = Linear(in_features=500, out_features=classes)
+		self.logSoftmax = LogSoftmax(dim=1)
+
+	def forward(self, x):
+		# pass the input through our first set of CONV => RELU =>
+		# POOL layers
+		x = self.conv1(x)
+		x = self.relu1(x)
+		x = self.maxpool1(x)
+		# pass the output from the previous layer through the second
+		# set of CONV => RELU => POOL layers
+		x = self.conv2(x)
+		x = self.relu2(x)
+		x = self.maxpool2(x)
+		# flatten the output from the previous layer and pass it
+		# through our only set of FC => RELU layers
+		x = flatten(x, 1)
+		x = self.fc1(x)
+		x  = self.relu3(x)
+		# pass the output to our softmax classifier to get our output
+		# predictions
+		x = self.fc2(x)
+		output = self.logSoftmax(x)
+		# return the output predictions
+		return output
