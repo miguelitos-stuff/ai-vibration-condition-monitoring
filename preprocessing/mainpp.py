422e516da1a1ce04f7b6532cfd39df180edd8a5a
flori <florian.mellab@gmail.com>
2023-03-21 10:29:20 +0100

    if name = main

--- preprocessing/mainpp.py
+++ preprocessing/mainpp.py
@@ -1,13 +1,25 @@
-#Main file loop for preprocessin
+#Main file loop for preprocessing
 
 #Import libraries
 import matplotlib.pyplot as plt
-from ppfuncs import visualize
-from ppfuncs import extract_data
 import numpy as np
 
+from ppfuncs import extract_data
+from ppfuncs import normalize
+from ppfuncs import create_matrix
+from ppfuncs import generate_sample
+from ppfuncs import visualize
 
-#Visualize the data matrix
-test_matrix = np.random.randint(low=0, high=256, size=(224, 224))
-visualize(test_matrix)
+
+if __name__ == "__main__":
+
+
+    pass
+
+
+
+
+# #Visualize the data matrix
+# test_matrix = np.random.randint(low=0, high=256, size=(224, 224))
+# visualize(test_matrix)
 
