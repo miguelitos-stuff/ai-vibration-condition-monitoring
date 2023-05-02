e7d643bc2f7ba003f810cdb3802fb8d939f1e883
Jeroeno26 <j.slagmolen@student.tudelft.nl>
2023-05-02 16:54:13 +0200

    Convnet Calculator

--- ConvNet Calculator.py
+++ ConvNet Calculator.py
@@ -8,13 +8,13 @@ def conv_output_shape(input_shape, kernel_size, stride, padding):
     return (batch_size, filter_count, out_height, out_width)
 
 # Input shape: (batch_size, in_channels, height, width)
-input_shape = (1, 10, 224, 224)
+input_shape = (1, 60, 60, 60)
 
 # Convolutional layer parameters
-filter_count = 60
-kernel_size = 5
-stride = 2
-padding = 0
+filter_count = 50
+kernel_size = 3
+stride = 1
+padding = 1
 
 # Calculate output shape
 output_shape = conv_output_shape(input_shape, kernel_size, stride, padding)
