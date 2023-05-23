95ad805109b4b3f159f567a2638b33063f19fc43
Jeroeno26 <j.slagmolen@student.tudelft.nl>
2023-05-23 14:30:24 +0200

    XAI test

--- XAI test 1.py
+++ XAI test 1.py
@@ -6,6 +6,7 @@ strides = (3, 9, 9)               # smaller = more fine-grained attribution but 
 target=208,                       # Labrador index in ImageNet
 sliding_window_shapes=(3,45, 45)  # choose size enough to change object appearance
 baselines = 0                     # values to occlude the image with. 0 corresponds to gray
+model = newCNN4()
 
 attribution_dog = occlusion.attribute(input_img,
                                        strides = strides,
