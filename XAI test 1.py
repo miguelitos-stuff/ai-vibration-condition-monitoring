1d4a826904b8095adb1f27780df29d2473baf824
Mell0ww <xMellowz@gmail.com>
2023-05-23 14:35:46 +0200

    chnage model

--- XAI test 1.py
+++ XAI test 1.py
@@ -1,22 +1,51 @@
 from captum.attr import Occlusion
+from cnn_architecture import newCNN4
+occlusion = Occlusion(newCNN4())
+import numpy as np
+from captum.attr import visualization as viz
 
-occlusion = Occlusion(model)
-
-strides = (3, 9, 9)               # smaller = more fine-grained attribution but slower
-target=208,                       # Labrador index in ImageNet
+strides = (3, 9, 9)               # smaller = more fine-grained attribution but slower                      # Labrador index in ImageNet
 sliding_window_shapes=(3,45, 45)  # choose size enough to change object appearance
+target = 0
 baselines = 0                     # values to occlude the image with. 0 corresponds to gray
 
-attribution_dog = occlusion.attribute(input_img,
+damaged_box = occlusion.attribute(input_img,
                                        strides = strides,
                                        target=target,
                                        sliding_window_shapes=sliding_window_shapes,
                                        baselines=baselines)
 
 
-target=283,                       # Persian cat index in ImageNet
-attribution_cat = occlusion.attribute(input_img,
+target=1,                       # Persian cat index in ImageNet
+healthy_box = occlusion.attribute(input_img,
                                        strides = strides,
                                        target=target,
                                        sliding_window_shapes=sliding_window_shapes,
                                        baselines=0)
+
+# Convert the compute attribution tensor into an image-like numpy array
+attribution_dog = np.transpose(damaged_box.squeeze().cpu().detach().numpy(), (1,2,0))
+
+vis_types = ["heat_map", "original_image"]
+vis_signs = ["all", "all"] # "positive", "negative", or "all" to show both
+# positive attribution indicates that the presence of the area increases the prediction score
+# negative attribution indicates distractor areas whose absence increases the score
+
+_ = viz.visualize_image_attr_multiple(attribution_dog,
+                                      np.array(center_crop(img)),
+                                      vis_types,
+                                      vis_signs,
+                                      ["attribution for dog", "image"],
+                                      show_colorbar = True
+                                     )
+
+
+attribution_cat = np.transpose(healthy_box.squeeze().cpu().detach().numpy(), (1,2,0))
+
+_ = viz.visualize_image_attr_multiple(attribution_cat,
+                                      np.array(center_crop(img)),
+                                      ["heat_map", "original_image"],
+                                      ["all", "all"], # positive/negative attribution or all
+                                      ["attribution for cat", "image"],
+                                      show_colorbar = True
+                                     )
